ce38c8cae9241e0f6ba9e5e307c101fb104cab91
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-01-30 23:04:49 +0100

    liste user ok

--- Quizz-Laravel/resources/views/admin/private/index.blade.php
+++ Quizz-Laravel/resources/views/admin/private/index.blade.php
@@ -1,20 +1,11 @@
 @extends('layouts.app')
 @section('title')
-<h1>coucou Admin</h1>
-
-
-<h1>Les réponses au questionnaire</h1>
+<h1>Utilisateurs inscrits sur le site</h1>
 <ul>
     @foreach($users as $user)
     <li>{{$user->name}}</li>
     <li>{{$user->email}}</li>
     @endforeach
 </ul>
-@endsection
 
-@section('principale')
-<div class="answerCard">
-    <h2>Vos réponses aux questionnaires: </h2>
-    <h3>Utilisateurs: {{ $users}}</h3>
-    </div>
-@endsection
+
