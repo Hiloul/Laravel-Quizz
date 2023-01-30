fe8dd0a83df46197c2799b7157cc023fda0fa498
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-01-30 23:08:30 +0100

    toutes les reponses des utilisateurs entrées

--- Quizz-Laravel/resources/views/admin/private/getFullUsersAnswers.blade.php
+++ Quizz-Laravel/resources/views/admin/private/getFullUsersAnswers.blade.php
@@ -1,21 +1,17 @@
 @extends('layouts.app')
 
 @section('title')
-<h1>coucou admin</h1>
 
-<h1>Les réponses au questionnaires de Tous les utilisateurs</h1>
+<h1>Toutes les réponses au questionnaires de tous les utilisateurs</h1>
 <ul>
     @foreach($answers as $answer)
     <li>{{$answer->answer1}}</li>
+    <li>{{$answer->answer2}}</li>
+    <li>{{$answer->answer3}}</li>
+    <li>{{$answer->answer4}}</li>
+    <li>{{$answer->answer5}}</li>
+    <li>{{$answer->email}}</li>
     @endforeach
 </ul>
-@endsection
-@section('title', 'L\'article '.$answers)
-@section('principale')
-<div class="answerCard">
-    <h2>Les réponses aux questionnaires: </h2>
-    <h3>{{ $answers}}</h3>
-    <h3> Votre email: {{ $answer->email}}</h3>
-    </div>
-@endsection
+
 
