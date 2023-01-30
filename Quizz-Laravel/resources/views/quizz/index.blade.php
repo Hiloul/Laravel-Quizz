c393a6795f7c6bc071bae21b683c846fad223333
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-01-30 16:28:49 +0100

    index renvoyé ss forme dobjet corriger ca

--- Quizz-Laravel/resources/views/quizz/index.blade.php
+++ Quizz-Laravel/resources/views/quizz/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('contenu')
+@section('title')
 <h1>Les réponses au questionnaire</h1>
 <ul>
     @foreach($answers as $answer)
@@ -8,3 +8,13 @@
     @endforeach
 </ul>
 @endsection
+@section('title', 'L\'article '.$answers)
+@section('principale')
+<div class="answerCard">
+    <h2>Vos réponses aux questionnaires: </h2>
+    <h3>Réponses: {{ $answers}}</h3>
+    <h3> Votre email: {{ $answer->email}}</h3>
+    </div>
+@endsection
+<h1>coucou user</h1>
+
