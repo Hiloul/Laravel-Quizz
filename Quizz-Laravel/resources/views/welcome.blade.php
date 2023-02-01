bdd86a217d8c5821db2b16994a16a344c14eb518
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-01 23:10:27 +0100

    nav bar user ok

--- Quizz-Laravel/resources/views/welcome.blade.php
+++ Quizz-Laravel/resources/views/welcome.blade.php
@@ -26,11 +26,10 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" 
-                        class="text-sm text-gray-700 dark:text-gray-500 underline">
-                        Dashboard</a>
-                        <a href="{{ url('/quizz/create') }}" 
-                        class="text-sm text-gray-700 dark:text-gray-500 underline">Répondre à un questionnaire</a>
+                        <!-- Navbar auth -->
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        <a href="{{ url('/quizz/create') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Répondre à un questionnaire</a>
+                        <a href="{{ url('/quizz') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Mes questionnaires</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Connexion</a>
 
