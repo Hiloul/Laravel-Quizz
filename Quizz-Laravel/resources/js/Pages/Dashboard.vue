0f8bf05f0af1e1f4e0359215889b7c72125aea8e
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-02 22:17:48 +0100

    dashvueinertia

--- Quizz-Laravel/resources/js/Pages/Dashboard.vue
+++ Quizz-Laravel/resources/js/Pages/Dashboard.vue
@@ -20,7 +20,6 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">Vous êtes connecté(e)</div>
                 </div>
@@ -35,5 +34,8 @@ import { Head } from '@inertiajs/vue3';
 nav{display: flex;
 justify-content: space-evenly;
 align-self: flex-start;}
+a:hover{
+    color: royalblue;
+}
 .py-12{height: 550px;}
 </style>
