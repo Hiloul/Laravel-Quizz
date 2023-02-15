4f4222e7970afb1dec589e40c1781a02a9634b38
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-15 21:43:33 +0100

    logo

--- Quizz-Laravel/resources/js/Pages/Welcome.vue
+++ Quizz-Laravel/resources/js/Pages/Welcome.vue
@@ -170,7 +170,7 @@ defineProps({
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-            <a href="/"><img src="Welcome.vue" alt=""></a>
+            <a href="/"><img src="" alt=""></a>
                 <h2 class="h-16 w-auto text-gray-700 sm:h-20">
                    Laravel Quizz</h2>
             </div>
