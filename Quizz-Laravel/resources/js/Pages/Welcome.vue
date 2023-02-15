b79a382054765168241ab76c7153480f9276020d
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-15 21:39:54 +0100

    ok

--- Quizz-Laravel/resources/js/Pages/Welcome.vue
+++ Quizz-Laravel/resources/js/Pages/Welcome.vue
@@ -224,8 +224,7 @@ defineProps({
                 </div>
             </div>
 
-            <footer>
-    
+        <footer>
     <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
         <div class="text-center text-sm text-gray-500 sm:text-left">
             <div class="flex items-center">
@@ -234,12 +233,10 @@ defineProps({
             </div>
         </div>
     </div>
-        <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+            <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
             Laravel Quizz&copy; Hilel 2023
-        </div>
-</footer>
-        </div>
+            </div>
+        </footer>
     </div>
-    
-        
+</div>    
 </template>
