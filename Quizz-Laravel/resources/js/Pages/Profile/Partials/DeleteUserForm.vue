5d331638e8f13ed2aff882f111fad15d02106b18
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-15 22:03:32 +0100

    trans ok

--- Quizz-Laravel/resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ Quizz-Laravel/resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -40,11 +40,12 @@ const closeModal = () => {
 <template>
     <section class="space-y-6">
         <header>
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Delete Account</h2>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Supprimer le compte</h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting
-                your account, please download any data or information that you wish to retain.
+                Supprimer votre compte entrainera la perte de toutes vos doonées, l'action sera définitive
+                et irreversible. Veillez à sauvegarder vos données.
+                
             </p>
         </header>
 
@@ -57,8 +58,7 @@ const closeModal = () => {
                 </h2>
 
                 <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Once your account is deleted, all of its resources and data will be permanently deleted. Please
-                    enter your password to confirm you would like to permanently delete your account.
+                   Supprimer votre compte sera définitif et irreversible. Veuillez entrer votre mot de passe pour confirmer.
                 </p>
 
                 <div class="mt-6">
