3c80035a15d57f03dcc714555eec671a29a8a46e
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-16 22:18:27 +0100

    route ok

--- Quizz-Laravel/routes/web.php
+++ Quizz-Laravel/routes/web.php
@@ -56,8 +56,7 @@ Route::get('/quizz', [QuizzController::class, 'index'])->name('quizz.index');
 Route::get('/quizz/create', [QuizzController::class, 'create'])->name('quizz.create')->where('id', '[0-9]+');
 Route::post('/quizz', [QuizzController::class, 'store'])->name('quizz.store');
 Route::get('/quizz/{id}', [QuizzController::class, 'show'])->name('quizz.show');
-Route::delete('/delete/{id}', [QuizzController::class, 'delete'])->name('quizz.delete');
-Route::get('delete/{id}','StudDeleteController@destroy');
+Route::get('/delete/{id}', [QuizzController::class, 'destroy'])->name('quizz.destroy');
 
         
 //Route::Update ?
