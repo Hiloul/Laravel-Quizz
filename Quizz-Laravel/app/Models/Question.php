1dd65903dd1ea5d5665fd5e5e119bffe1832df18
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-17 22:07:35 +0100

    question model

--- Quizz-Laravel/app/Models/Question.php
+++ Quizz-Laravel/app/Models/Question.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function questionOptions(){
+        return $this->hasMany(Option::class);
+    }
 }
