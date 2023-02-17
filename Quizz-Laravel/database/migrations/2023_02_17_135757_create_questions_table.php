b7b795046297c129c9c48ed3705688d35ad19a69
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-17 22:32:39 +0100

    soucis foreign key

--- Quizz-Laravel/database/migrations/2023_02_17_135757_create_questions_table.php
+++ Quizz-Laravel/database/migrations/2023_02_17_135757_create_questions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
 
 return new class extends Migration
 {
@@ -15,7 +16,8 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Category::class)->constrained()->cascadeOnUpdate();
+            //RELATION A CORRIGER AUSSI
+            // $table->foreignIdFor(Category::class)->constrained()->cascadeOnUpdate();
             $table->longText('question_text');
             $table->timestamps();
         });
