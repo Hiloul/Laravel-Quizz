aa165c0b527946f9ba93c31a0cf333d9c4211022
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-19 19:56:25 +0100

    basculé les foreign key sur role user

--- Quizz-Laravel/database/migrations/2023_01_25_230103_create_role_user_table.php
+++ Quizz-Laravel/database/migrations/2023_01_25_230103_create_role_user_table.php
@@ -20,6 +20,12 @@ return new class extends Migration
             $table->foreignIdFor(User::class);
             $table->foreignIdFor(Role::class);
         });
+        Schema::table('users', function(Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+        Schema::table('roles', function(Blueprint $table) {
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
     }
 
     /**
