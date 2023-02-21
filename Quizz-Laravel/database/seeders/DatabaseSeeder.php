2bba715795194fa37021a44afc97cd334d9d4afa
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-21 19:11:50 +0100

    normalement ok

--- Quizz-Laravel/database/seeders/DatabaseSeeder.php
+++ Quizz-Laravel/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
 use \App\Models\Role;
+use Database\Factories\RoleFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,12 +18,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
-
-        // User::factory(10)->create()->each(function($user){
-        //     Role::factory(rand(1,2))->create([
-        //     'user_id' => $user->id,
-        //     'role_id' => ($user->random(1)->first())->id
-        // ]);
-        // });
+        
     }
 }
