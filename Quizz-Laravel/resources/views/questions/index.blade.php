69070fcfc335f49eb2586df32250e15f00f219f2
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-02-23 20:56:24 +0100

    des

--- Quizz-Laravel/resources/views/questions/index.blade.php
+++ Quizz-Laravel/resources/views/questions/index.blade.php
@@ -8,76 +8,63 @@
 <a href="/questions">Questions</a>
 </nav>
 <div class="container-fluid">
-
-    <!-- Page Heading -->
-   
-
-    <!-- Content Row -->
-        <div class="card">
-            <div class="card-header py-3 d-flex">
-                <h6 class="m-0 font-weight-bold text-primary">
-                    {{ __('Question(s)') }}
-                </h6>
-                <div class="ml-auto">
-                    <a href="{{ route('questions.create') }}" class="btn btn-primary">
-                        <span class="icon text-white-50">
-                            <i class="fa fa-plus"></i>
-                        </span>
-                        <span class="text">{{ __('Créer une nouvelle question') }}</span>
-                    </a>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped table-hover datatable datatable-question" cellspacing="0" width="100%">
-                        <thead>
-                            <tr>
-                                <th width="10">
-
-                                </th>
-                                <th>Numéro</th>
-                                <th>Catégorie</th>
-                                <th>Question</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($questions as $question)
-                            <tr data-entry-id="{{ $question->id }}">
-                                <td>
-
-                                </td>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $question->category->name }}</td>
-                                <td>{{ $question->question_text }}</td>
-                                <td>
-                                    <div class="btn-group btn-group-sm">
-                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-info">
-                                            Modifier
-                                        </a>
-                                        <form onclick="return confirm('are you sure ? ')" class="d-inline" action="{{ route('questions.destroy', $question->id) }}" method="POST">
-                                            @csrf
-                                            @method('delete')
-                                            <button class="btn btn-danger" style="border-top-left-radius: 0;border-bottom-left-radius: 0;">
-                                                Supprimer
-                                            </button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                            @empty
-                            <tr>
-                                <td colspan="7" class="text-center">{{ __('Aucunes données entrées') }}</td>
-                            </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
+    <div class="card">
+        <div class="card-header py-3 d-flex">
+            <h6 class="m-0 font-weight-bold text-primary">
+                {{ __('Question(s)') }}
+            </h6>
+            <div class="ml-auto">
+                <a href="{{ route('questions.create') }}" class="btn btn-primary">
+                <span class="icon text-white-50"> + </span>
+                <span class="text">{{ __('Créer une nouvelle question') }}</span>
+                </a>
             </div>
         </div>
-    <!-- Content Row -->
+    <div class="card-body">
+        <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover datatable datatable-question" cellspacing="0" width="100%">
+            <thead>
+            <tr>
+            <th width="10">
+            </th>
+            <th>Numéro</th>
+            <th>Catégorie</th>
+            <th>Question</th>
+            <th>Action</th>
+            </tr>
+            </thead>
+<tbody>
+@forelse($questions as $question)
+<tr data-entry-id="{{ $question->id }}">
+<td>
 
+</td>
+<td>{{ $loop->iteration }}</td>
+<td>{{ $question->category->name }}</td>
+<td>{{ $question->question_text }}</td>
+<td>
+    <div class="btn-group btn-group-sm">
+        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-info"> Modifier </a>
+        <form onclick="return confirm('Etes vous sure ? ')" class="d-inline" action="{{ route('questions.destroy', $question->id) }}" method="POST">
+        @csrf
+        @method('delete')
+        <button class="btn btn-danger" style="border-top-left-radius: 0;border-bottom-left-radius: 0;"> Supprimer </button>
+        </form>
+    </div>
+</td>
+</tr>
+@empty
+<tr>
+<td colspan="7" class="text-center">{{ __('Aucunes données entrées') }}</td>
+</tr>
+@endforelse
+</tbody>
+</table>
 </div>
+    </div>
+        </div>
+</div> 
+<!-- Footer -->
 <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
     Laravel Quizz&copy; Hilel 2023
 </div>
