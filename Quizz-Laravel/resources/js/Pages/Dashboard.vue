98be269ffde859edd9cf75c55990b3f0f3fc6d3b
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-03-10 22:55:17 +0100

    test

--- Quizz-Laravel/resources/js/Pages/Dashboard.vue
+++ Quizz-Laravel/resources/js/Pages/Dashboard.vue
@@ -18,6 +18,18 @@ import { Head } from '@inertiajs/vue3';
             </h2>
         </template>
 
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
+                
+            <template v-else>
+                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Connexion</Link>
+
+                <Link v-if="canRegister" :href="route('register')"
+                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Inscription</Link>
+            </template>
+        </div>
+
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
