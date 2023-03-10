143d5b460f38ce559b8b5e76ca2b26f6ec26e772
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-03-10 22:49:59 +0100

    10

--- Quizz-Laravel/resources/js/Pages/Dashboard.vue
+++ Quizz-Laravel/resources/js/Pages/Dashboard.vue
@@ -23,7 +23,8 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Vous êtes connecté(e)</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100"> <a href="/questions">Créer</a>
+                    <a href="/options">Répondre</a></div>
                 </div>
             </div>
         </div>
