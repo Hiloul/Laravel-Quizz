2945086b7b7526761757be5aa0f91b69bec14a8e
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-03-10 22:50:27 +0100

    br

--- Quizz-Laravel/resources/js/Pages/Dashboard.vue
+++ Quizz-Laravel/resources/js/Pages/Dashboard.vue
@@ -23,8 +23,10 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100"> <a href="/questions">Créer</a>
-                    <a href="/options">Répondre</a></div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100"> 
+                        <a href="/questions">Créer</a>
+                        <br>
+                        <a href="/options">Répondre</a></div>
                 </div>
             </div>
         </div>
